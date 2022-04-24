0c060dc666adc3e8c12aadc5e115925a46f1d302
James Kaloki <Kaloki@jannettes-MacBook-Air.local>
2022-04-25 01:16:54 +0300

    defined the __init method that created properties for objects

--- Credentiallocker.py
+++ Credentiallocker.py
@@ -2,4 +2,17 @@ import random
 import string 
 
 class Credentials:
-    pass
+    '''
+    class that generates new credentials
+    '''
+    credentials_list=[]
+    def __init__(self):
+        '''
+         __init method that helps us define properties for our objects
+        Args:
+        application_name: New application name
+        account_username: New account username
+        account_password: New account password
+        '''
+        pass
+    
