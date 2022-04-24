2f5d73333cef9fc1edf201d1da537dbb0239ffa0
James Kaloki <Kaloki@jannettes-MacBook-Air.local>
2022-04-24 22:17:11 +0300

    created an array to store the passwords

--- Passwordlocker.py
+++ Passwordlocker.py
@@ -1,3 +1,7 @@
 class PasswordLocker:
+    Credentials=[]
     def __init__(self,username,password) :
-        pass
+        'Creates new password and username when initiallized'
+        self.username=username
+        self.password=password
+
