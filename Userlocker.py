e4873fbd104a3e326e2a4c8ba4c6a6c20ce8429b
James Kaloki <Kaloki@jannettes-MacBook-Air.local>
2022-04-25 01:08:59 +0300

    created a function user_exist to check if a user exits in the user.list array

--- Userlocker.py
+++ Userlocker.py
@@ -26,3 +26,17 @@ class Users:
         for user in Users.users_list:
             if user.username == username:
                 return user
+    @classmethod
+    def user_exists(cls, username, login_password):
+        '''
+        authenticate user username and password by checking if user exists in the users list
+        Args: 
+        username : name used by user to login
+        login_password: password for the user
+        Returns: 
+        boolean
+        '''
+        for user in Users.users_list:
+            if user.username == username and user.login_password == login_password:
+                return True
+        return False
