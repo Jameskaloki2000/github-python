c13e3c231554176d22786ce3b4620d68e647cf44
James Kaloki <Kaloki@jannettes-MacBook-Air.local>
2022-04-24 22:22:05 +0300

    created the save password defintion to save passwords

--- Passwordlocker.py
+++ Passwordlocker.py
@@ -4,4 +4,6 @@ class PasswordLocker:
         'Creates new password and username when initiallized'
         self.username=username
         self.password=password
-
+    def save_password (self):
+        PasswordLocker.Credentials.append(self)
+        "A  deifintion that takes in the self object and pushed it to the created array to store the passord"
