43cd0f904b009ec5e217ece447e5bed47df2c235
James Kaloki <Kaloki@jannettes-MacBook-Air.local>
2022-04-25 01:01:38 +0300

    changed array name for userlocker.py and the init function arguements name

--- Userlocker.py
+++ Userlocker.py
@@ -1,9 +1,10 @@
-class PasswordLocker:
-    userCredentials=[]
-    def __init__(self,username,password) :
+class Users:
+    user_list=[]
+    def __init__(self,username,login_password) :
         'Creates new password and username when initiallized'
         self.username=username
-        self.password=password
+        self.password=login_password
     def save_password (self):
-        PasswordLocker.Credentials.append(self)
+        Users. user_login.append(self)
         "A  deifintion that takes in the self object and pushed it to the created array to store the passord"
+    
